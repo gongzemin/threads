0aa256e743c69501ca2e4b9ba57d4b0f43df1de9
mingongze <andersonmingz@gmail.com>
2023-08-09 19:03:25 +0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: mingongze andersonmingz@gmail.com
+ * @Date: 2023-08-09 17:20:15
+ * @LastEditors: mingongze andersonmingz@gmail.com
+ * @LastEditTime: 2023-08-09 17:26:23
+ * @FilePath: \threads\app\page.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Threds</h1>
+    </main>
+  )
+}
